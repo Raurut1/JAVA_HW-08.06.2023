57a060adb7f8b8afb091d825c90c6f429037eac3
Raurut <makey@yandex.ru>
2023-06-08 03:47:50 +0300

    Task 1 complete

---
+++ Taskone.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Taskone {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int i = random.nextInt(201);
+        System.err.println(i);
+
+    }
+  }
